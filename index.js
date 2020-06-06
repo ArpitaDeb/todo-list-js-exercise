775df2e7571073c701db8bd0bad3c83bf3f1f60e
ArpitaDeb <arpitadeb1990@gmail.com>
2020-06-06 01:30:08 +0000

    todo practice using object method

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
+//Using Object methods
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logState: function () {
+      console.log(`${this.title} has ${this.complete ? " " : " not "} been completed`);
+    },
+
+    markCompleted: function () {
+      this.complete = true;
+    }
+  };
+  return task;
+}
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+task1.logState();
+task1.markCompleted();
+task1.logState();
+
+/* using Function refactoring
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false
+  };
+  return task;
+}
+// prints out the provided task's details
+function logTaskState(task) {
+  console.log(`${task.title} has ${task.complete ? " " : " not "} been completed`);
+}
+// marks the provided task as completed
+function completeTask(task) {
+  task.complete = true;
+}
+// DRIVER CODE BELOW
+
+logTaskState(task1);
+completeTask(task1);
+logTaskState(task1);
+console.log(tasks);
+
 // Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
+const taskDescriptions = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
+function newTask(title, description) {
   taskTitles.push(title);
+  taskDescriptions.push(description);
   taskComplete.push(false);
 }
 
@@ -20,8 +71,7 @@ function logTaskState(taskIndex) {
   const complete = taskComplete[taskIndex];
   console.log(`${title} has${complete ? " " : " not "}been completed`);
 }
-
-// DRIVER CODE BELOW
+ DRIVER CODE BELOW
 
 newTask("Clean Cat Litter"); // task 0
 newTask("Do Laundry"); // task 1
@@ -29,3 +79,4 @@ newTask("Do Laundry"); // task 1
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
 logTaskState(0); // Clean Cat Litter has been completed
+*/
